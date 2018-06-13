b39d76ce25a84fe14a71dd8f376b5b56f94c4616
Matt <matt.timmermans@student.fontys.nl>
2018-06-13 15:23:19 +0200

    Version 1.1 All SQL and XSS statements are made. Next step: Let it send a mail when there is an attack and say what kind of attack

--- detector.js
+++ detector.js
@@ -9,11 +9,20 @@ function detectAttack() {
     for (var i = 0, len = count.length; i < len; i++) {
         var gegeven = count[i].value;
         // Check for SQL injection
-        if (gegeven.indexOf("'" || "#" || ";" || "=") >= 0) {
-            document.write("SQL INJECTION");
-
-        } else if (gegeven.indexOf("<img" || "<b" || "<u" || "<script") >= 0) {
-            document.write("XSS");
+        if (gegeven.indexOf("'") >= 0) {
+            document.write("Error Based SQL Injection (')");
+        } else if (gegeven.indexOf("&&") >= 0) {
+            document.write("Encoded Boolean Based SQL Injection (&&)");
+        } else if (gegeven.indexOf("||") >= 0) {
+            document.write("Encoded Boolean Based SQL Injection (||)");
+        } else if (gegeven.indexOf("||") >= 0) {
+            document.write("Encoded Boolean Based SQL Injection (||)");
+        } else if (gegeven.indexOf("<script") >= 0) {
+            document.write("SCRIPT XSS Attack (SCRIPT)");
+        } else if (gegeven.indexOf("<img") >= 0) {
+            document.write("IMG XSS Attack (IMG)");
+        } else if (gegeven.indexOf("<") >= 0) {
+            document.write("Other XSS Attack (<)");
         } else{
             // Nothing (deleted)
         }
